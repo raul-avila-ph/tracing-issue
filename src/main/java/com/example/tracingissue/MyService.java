a70ab5871e0249ab16d9aa8a75518bc024b95f75
Raul Avila <raul.avila@pagonxt.com>
2024-03-27 14:49:23 +0000

    Tracing issue not happening - Without FactoryBean

--- src/main/java/com/example/tracingissue/MyService.java
+++ src/main/java/com/example/tracingissue/MyService.java
@@ -2,7 +2,9 @@ package com.example.tracingissue;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+@Service
 @Slf4j
 @RequiredArgsConstructor
 public class MyService {
